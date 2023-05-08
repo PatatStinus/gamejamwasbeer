06812dbd58c13869c904cf9f4092de7d071e57d7
idsscholte <90754772+idsscholte@users.noreply.github.com>
2023-05-08 14:07:37 +0200

    main menu working

---
+++ Assets/Ids/Scripts/Buttons.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Buttons : MonoBehaviour
+{
+    public GameObject settings;
+    public GameObject MainMenu;
+
+    private void Start()
+    {
+        settings.SetActive(false);
+    }
+
+    public void Settings() 
+    {
+        settings.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+    public void SettingsOff()
+    {
+        settings.SetActive(false);
+        MainMenu.SetActive(true);
+
+    }
+
+    public void Quit() 
+    {
+        Application.Quit();
+    }
+}
